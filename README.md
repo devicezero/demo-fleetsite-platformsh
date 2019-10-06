61549611708443efc7ee9b8b9d61bacacd18f3ed
Jonas Kroeger <jk@devicezero.me>
2019-10-06 17:19:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cloned History Test Project
---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Messages;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$messages = new Messages();
+
+printf("<h1>%s</h1>\n", $messages->title());
+printf("<p>%s</p>\n", $messages->message());
+
+printf("<p>Count: %s</p>\n", 6);
